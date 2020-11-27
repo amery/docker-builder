76ec9d2a21c2d645e55e430550f14026a620fd07
Alejandro Mery <amery@jpi.io>
2020-11-27 02:51:59 +0000

    golang: refactor run.sh using generic library (docker_builder_run.in)
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/golang/latest/run.sh
+++ docker/golang/latest/run.sh
@@ -2,33 +2,54 @@
 
 set -eu
 
-if [ -z "${GOPATH:-}" ]; then
-	GOPATH=$PWD
-	mkdir -p "$PWD/bin" "$PWD/src" "$PWD/pkg"
-fi
+# Variables:
+#   DOCKER_DIR           ${DOCKER_DIR}/Dockerfile
+#   DOCKER_ID            optional image id to use instead of building DOCKER_DIR
+#   DOCKER_EXTRA_OPTS    extra options to pass as-is to `docker run`
+#   DOCKER_RUN_CACHE_DIR
+#   DOCKER_RUN_VOLUMES
+#   DOCKER_RUN_ENV
+#
+#   GOPATH
+#
 
-if [ -n "$(docker --version 2> /dev/null)" ]; then
-	DOCKER_DIR="$(dirname "$(readlink -f "$0")")"
+RUN_SH=$(readlink -f "$0")
+. "$(dirname "$RUN_SH")/../../docker_builder_run.in"
 
-	docker build --rm "$DOCKER_DIR"
-	DOCKER_ID="$(docker build -q --rm "$DOCKER_DIR")"
-	set -- \
-		-e USER_NAME="$(id -urn)" \
-		-e USER_UID="$(id -ur)" \
-		-e USER_GID="$(id -gr)" \
-		${DOCKER_EXPOSE:+-p $DOCKER_EXPOSE:$DOCKER_EXPOSE} \
-		-v "$GOPATH:/go" \
-		"$DOCKER_ID" "$@"
+# DOCKER_ID
+if [ -z "$(docker --version 2> /dev/null)" ]; then
+	DOCKER_ID=
+	DOCKER_DIR=
+elif [ -z "${DOCKER_ID:-}" ]; then
+	DOCKER_DIR="$(builder_find_docker_dir "$0" "${DOCKER_DIR:-}")"
 
-	if [ -t 0 ]; then
-		set -- -ti "$@"
-	else
-		set -- -i "$@"
+	if [ -d "$DOCKER_DIR" ]; then
+		docker build --rm "$DOCKER_DIR"
+		DOCKER_ID="$(docker build -q --rm "$DOCKER_DIR")"
 	fi
-
-	set -- docker run --rm "$@"
-elif [ $# -eq 0 ]; then
-	set -- ${SHELL:-/bin/sh}
 fi
 
-exec "$@"
+# take GOPATH as the root of the workspace
+#
+if [ -z "${GOPATH:-}" ]; then
+	# find GOPATH
+	#
+	test_gopath() {
+		test -d "$1/pkg"
+	}
+
+	GOPATH=$(builder_find_workspace test_gopath)
+fi
+
+mkdir -p "$GOPATH/bin" "$GOPATH/src" "$GOPATH/pkg"
+
+# run
+#
+[ $# -gt 0 ] || set -- ${SHELL:-/bin/sh}
+
+if [ -n "$DOCKER_ID" ]; then
+	builder_run_exec "$GOPATH" -v "$GOPATH:/go" "$DOCKER_ID" "$@"
+else
+	export GOPATH
+	exec "$@"
+fi
