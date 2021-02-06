6624dcaae343925e5245f4c968ff023d5ba73161
Alejandro Mery <amery@jpi.io>
2021-02-06 18:24:14 +0000

    docker-poky-builder: request env vars via LABEL
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/poky/latest/run.sh
+++ docker/poky/latest/run.sh
@@ -3,7 +3,6 @@
 RUN_SH="$(readlink -f "$0")"
 export DOCKER_DIR="${RUN_SH%/*}"
 
-export DOCKER_RUN_ENV="MACHINE DISTRO TCLIBC OEROOT DL_DIR"
 export DOCKER_RUN_VOLUMES="DL_DIR"
 
 exec "${DOCKER_DIR}/../../run.sh" "$@"
