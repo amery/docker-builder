fe85b189569d6cc3cdd61ed93ef9f823ea2eb5f3
Alejandro Mery <amery@jpi.io>
2021-02-19 21:20:35 +0000

    docker-nodejs-builder: include yarn version in tag
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/nodejs/latest/builder_version.sh
+++ docker/nodejs/latest/builder_version.sh
@@ -6,11 +6,21 @@ OS_V2=$(cut -d. -f1-2 /etc/alpine-release)
 
 NODE_VERSION="$(node --version | sed -e 's/^v//')"
 NPM_VERSION="$(npm info npm version)"
+YARN_VERSION="$(yarn -version)"
 
 NODE_V2=$(echo "$NODE_VERSION" | cut -d. -f1-2)
+YARN_V2=$(echo "$YARN_VERSION" | cut -d. -f1-2)
+NPM_V2=$(echo "$NPM_VERSION" | cut -d. -f1-2)
 NPM_V1=$(echo "$NPM_VERSION" | cut -d. -f1)
 
-echo "$NODE_VERSION-npm$NPM_VERSION-alpine$OS_V2"
-echo "$NODE_VERSION-npm$NPM_VERSION"
-echo "$NODE_VERSION-npm$NPM_V1"
+echo "$NODE_VERSION-npm$NPM_VERSION-yarn$YARN_VERSION-alpine$OS_V2"
+
+echo "$NODE_V2-npm$NPM_V2-yarn$YARN_V2-alpine$OS_V2"
+echo "$NODE_V2-npm$NPM_V2-yarn$YARN_V2"
+
+echo "$NODE_V2-npm$NPM_V2-alpine$OS_V2"
+echo "$NODE_V2-npm$NPM_V2"
 echo "$NODE_V2-npm$NPM_V1"
+
+echo "$NODE_V2-yarn$YARN_V2-alpine$OS_V2"
+echo "$NODE_V2-yarn$YARN_V2"
