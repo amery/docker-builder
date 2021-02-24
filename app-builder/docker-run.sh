84f35b50392d32ef5337e74e0cc89227441bc968
Alejandro Mery <amery@geeks.cl>
2021-02-24 15:56:58 +0000

    app-builder: pass --root/--version through
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- app-builder/docker-run.sh
+++ app-builder/docker-run.sh
@@ -6,7 +6,15 @@ ARG0="$(readlink -f "$0")"
 SCRIPTS_DIR="$(dirname "$ARG0")"
 WS=$(dirname "$0")
 
-set -- "$SCRIPTS_DIR/run.sh" "$@"
+case "${1:-}" in
+	--root|--version|"")
+		# skip run.sh
+		;;
+	*)
+		set -- "$SCRIPTS_DIR/run.sh" "$@"
+		;;
+esac
+
 if [ -x "$(which docker)" ]; then
 	exec "$WS/docker-run.sh" "$@"
 else
