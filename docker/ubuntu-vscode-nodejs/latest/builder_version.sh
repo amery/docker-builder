feee4994f53d3336eccdcec632bd374e9bc7d1d8
Alejandro Mery <amery@jpi.io>
2021-11-06 04:50:15 +0000

    docker/ubuntu-vscode-nodejs/latest: fix builder_version.sh
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/ubuntu-vscode-nodejs/latest/builder_version.sh
+++ docker/ubuntu-vscode-nodejs/latest/builder_version.sh
@@ -5,7 +5,7 @@ set -eu
 . /etc/os-release
 
 APP=vscode
-APP_VERSION="$(dpkg -s vscode | sed -n 's|^Version: \([^-]\+\).*|\1|p')"
+APP_VERSION="$(dpkg -s code | sed -n 's|^Version: \([^-]\+\).*|\1|p')"
 
 NODE_VERSION="$(node --version | sed -e 's/^v//')"
 NPM_VERSION="$(npm info npm version)"
@@ -19,7 +19,7 @@ NPM_V1=$(echo "$NPM_VERSION" | cut -d. -f1)
 echo "$APP$APP_VERSION-node$NODE_VERSION-npm$NPM_V2-yarn$YARN_V2-ubuntu$VERSION_ID"
 
 echo "$APP$APP_VERSION-node$NODE_V2-npm$NPM_V2-yarn$YARN_V2-ubuntu$VERSION_ID"
-echo "$APP$APP_V2-node$NODE_V2-npm$NPM_V2-yarn$YARN_V2"
+echo "$APP$APP_VERSION-node$NODE_V2-npm$NPM_V2-yarn$YARN_V2"
 
 echo "$APP$APP_VERSION-node$NODE_V2-npm$NPM_V2-ubuntu$VERSION_ID"
 echo "$APP$APP_VERSION-node$NODE_V2-npm$NPM_V2"
