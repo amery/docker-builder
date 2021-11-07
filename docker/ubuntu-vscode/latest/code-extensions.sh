a414b8f335087b4c0537a2cc1c2310a14440959a
Alejandro Mery <amery@jpi.io>
2021-11-07 21:41:06 +0000

    docker/ubuntu-vscode: add code_install_extension entrypoint helper
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ docker/ubuntu-vscode/latest/code-extensions.sh
@@ -0,0 +1,17 @@
+#
+# vscode extensions helper
+#
+code_install_extension() {
+	local x= pat=
+	for x; do
+		pat="$(echo "$x" | sed -e 's|\.|\\.|g')"
+
+	cat <<EOT
+# $x
+echo "[code:$x]"
+if ! code --list-extensions | grep -q -e "^$pat\$"; then
+	code --install-extension $x
+fi
+EOT
+	done
+}
--- docker/ubuntu-vscode/latest/code-vim.sh
+++ docker/ubuntu-vscode/latest/code-vim.sh
@@ -1,6 +1,1 @@
-cat <<EOT
-# vim support for vscode
-if ! code --list-extensions | grep -q -e '^vscodevim\.vim$'; then
-	code --install-extension vscodevim.vim
-fi
-EOT
+code_install_extension vscodevim.vim
