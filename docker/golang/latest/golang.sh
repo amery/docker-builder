f3cf40c27e3d1689ff4755b0fc755856329763cc
Alejandro Mery <amery@jpi.io>
2021-11-22 19:04:08 +0000

    golang: don't force `/usr/local/go/bin` onto $PATH
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/golang/latest/golang.sh
+++ docker/golang/latest/golang.sh
@@ -1,7 +1,6 @@
 cat <<EOT
 
 export GOPATH="${GOPATH:-$WS}"
-export PATH="/usr/local/go/bin:\$PATH"
 [ "x$GOPATH" = "x$WS" ] || export PATH="\$GOPATH/bin:\$PATH"
 export CGO_ENABLED=0
 ${GO111MODULE:+export GO111MODULE=${GO111MODULE}
