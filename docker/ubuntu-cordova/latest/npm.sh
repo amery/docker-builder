4f8193c60c1517aafa23aabd7c5fe42af5ae2a0d
Alejandro Mery <amery@jpi.io>
2021-11-22 18:44:18 +0000

    docker: add npm_needs entrypoint helper doing `npm i -g` if needed
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/ubuntu-cordova/latest/npm.sh
+++ docker/ubuntu-cordova/latest/npm.sh
@@ -1,3 +1,16 @@
 cat <<EOT
+
 export NPM_CONFIG_PREFIX="${NPM_CONFIG_PREFIX:-$WS}"
 EOT
+
+npm_needs() {
+	if [ $# -gt 0 ]; then
+		cat <<EOT
+
+if ! type -p $1 2> /dev/null >&2; then
+	echo "+ npm i -g $*" >&2
+	npm i -g $*
+fi
+EOT
+	fi
+}
