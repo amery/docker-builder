83747968385dc198a5c8357d0712a026b9b886ed
Alejandro Mery <amery@jpi.io>
2022-05-11 15:57:54 +0000

    docker-nodejs-builder: updated current to 18-alpine15
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/nodejs/current/builder_version.sh
+++ docker/nodejs/current/builder_version.sh
@@ -9,6 +9,7 @@ NPM_VERSION="$(npm info npm version)"
 YARN_VERSION="$(yarn -version)"
 
 NODE_V2=$(echo "$NODE_VERSION" | cut -d. -f1-2)
+NODE_V1=$(echo "$NODE_VERSION" | cut -d. -f1)
 YARN_V2=$(echo "$YARN_VERSION" | cut -d. -f1-2)
 NPM_V2=$(echo "$NPM_VERSION" | cut -d. -f1-2)
 NPM_V1=$(echo "$NPM_VERSION" | cut -d. -f1)
@@ -20,7 +21,7 @@ echo "$NODE_V2-npm$NPM_V2-yarn$YARN_V2"
 
 echo "$NODE_V2-npm$NPM_V2-alpine$OS_V2"
 echo "$NODE_V2-npm$NPM_V2"
-echo "$NODE_V2-npm$NPM_V1"
+echo "$NODE_V1-npm$NPM_V1"
 
 echo "$NODE_V2-yarn$YARN_V2-alpine$OS_V2"
 echo "$NODE_V2-yarn$YARN_V2"
