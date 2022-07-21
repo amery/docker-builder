713cb9c1bb13b1a9d2416f5e0eee085494a8ddad
Alejandro Mery <amery@jpi.io>
2022-07-21 22:59:20 +0000

    docker-ubuntu-nodejs-golang-builder: add `nvm` support
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/ubuntu-nodejs-golang/22.04/node.sh
+++ docker/ubuntu-nodejs-golang/22.04/node.sh
@@ -1,6 +1,18 @@
 cat <<EOT
 
-export NPM_CONFIG_PREFIX="${NPM_CONFIG_PREFIX:-$WS}"
+if [ -s "\$HOME/.nvm/nvm.sh" ]; then
+	unset NPM_CONFIG_PREFIX
+	export -n NPM_CONFIG_PREFIX || true
+
+	export NVM_DIR="\$HOME/.nvm"
+	. "\$NVM_DIR/nvm.sh"
+
+	if [ -s "\$NVM_DIR/bash_completion" ]; then
+		. "\$NVM_DIR/bash_completion"
+	fi
+else
+	export NPM_CONFIG_PREFIX="${NPM_CONFIG_PREFIX:-$WS}"
+fi
 EOT
 
 npm_needs() {
