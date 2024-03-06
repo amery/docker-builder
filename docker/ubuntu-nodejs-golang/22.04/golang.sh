fc93ae58a1a5577ca050f51a704b4acd356f108a
Alejandro Mery <amery@jpi.io>
2024-03-06 15:45:22 +0000

    golang: defer resolution of GOPATH on /etc/profile
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/ubuntu-nodejs-golang/22.04/golang.sh
+++ docker/ubuntu-nodejs-golang/22.04/golang.sh
@@ -1,7 +1,7 @@
 cat <<EOT
-
-export GOPATH="${GOPATH:-$WS}"
-[ "x$GOPATH" = "x$WS" ] || export PATH="\$GOPATH/bin:\$PATH"
+# Go
+export GOPATH="\${GOPATH:-\${WS:-\$HOME/go}}"
+[ "x\$GOPATH" = "\${WS:-}" ] || export PATH="\$GOPATH/bin:\$PATH"
 export CGO_ENABLED=0
 ${GO111MODULE:+export GO111MODULE=${GO111MODULE}
 }${GOINSECURE:+export GOINSECURE=${GOINSECURE}
