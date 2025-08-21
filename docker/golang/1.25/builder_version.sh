a664873659deb95c3897eaf7d185217d70076b2b
Alejandro Mery <amery@apptly.co>
2025-08-21 08:01:11 +0000

    docker-golang-builder: add 1.25.0 image
    Signed-off-by: Alejandro Mery <amery@apptly.co>

---
+++ docker/golang/1.25/builder_version.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -eu
+
+OS_V2=$(cut -d. -f1-2 /etc/alpine-release)
+
+GO_VERSION=$(go version | sed -e 's|.* go\([1-9][^ ]\+\) .*|\1|')
+GO_V2=$(echo "$GO_VERSION" | cut -d. -f1-2)
+
+echo "$GO_VERSION-alpine$OS_V2"
+echo "$GO_V2-alpine$OS_V2"
